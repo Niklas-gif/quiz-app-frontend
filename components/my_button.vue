a3dfaf1d523a3f2017152e74dccd67b868c6ab34
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-09 19:42:47 +0200

    data binding example

---
+++ components/my_button.vue
@@ -0,0 +1,26 @@
+//Test
+<template>
+    <input v-model="message"/>
+    <button 
+        @click="submitChanges" 
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        Submit
+    </button>
+    <p>{{display}}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "",
+            display: "Hello, Vue!"
+        };
+    },
+    methods: {
+        submitChanges() {
+            this.display = this.message;
+        }
+    }
+};
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,7 @@
 <NuxtPage/>
 <h1>About</h1>
 <nav>
+    <MyButton></MyButton>
     <RouterLink to="/">Home</RouterLink>
 </nav>
 </template>
