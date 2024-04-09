3e42df96cbe9a0faf2257f414b3bd722d6a2b825
Niklas D <niklas.demel1@gmail.com>
2024-04-09 13:12:33 +0200

    Routerlink vs a

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+<NuxtPage/>
+<h1>About</h1>
+<nav>
+    <RouterLink to="/">Home</RouterLink>
+</nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
 <NuxtLayout>
     <NuxtWelcome/>
+    <nav>
+        <RouterLink to="/about">About (RoutLink)</RouterLink>
+        <br>
+        <a href="/about">About (pageload)</a>
+    </nav>
 </NuxtLayout>
 </template>
