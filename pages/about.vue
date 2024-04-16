7a0bb96dcf9d80152baad6fe99b0446eabb74be4
Niklas D <niklas.demel1@gmail.com>
2024-04-16 09:09:25 +0200

    play around and find out

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,31 @@
 <template>
 <NuxtPage/>
 <h1>About</h1>
+<button @click="toggle_button">
+    test
+</button>
+<div v-if="show_button">
+    <h1 class="font-bold">Test</h1>
+</div>
 <nav>
     <MyButton></MyButton>
     <RouterLink to="/">Home</RouterLink>
 </nav>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+        show_button: true
+        };
+    },
+    methods: {
+        toggle_button() {
+            this.show_button=!this.show_button
+        }
+    }
+
+};
+
+</script>
