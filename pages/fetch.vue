581e1dc21cceac17bfe988072b248b7c1b137f1b
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-28 15:47:30 +0200

    test fetch

---
+++ pages/fetch.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    {{ dataTwice }}
+</div>
+</template>
+
+<script setup>
+
+//const dataTwice = await $fetch('https://jsonplaceholder.typicode.com/posts')
+const dataTwice = await $fetch('http://localhost:3030/quizzes')
+//http://localhost:3030/ping
+
+</script>
