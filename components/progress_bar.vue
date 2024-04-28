500a96806951746c04f3ca884f8532dc508a064b
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-28 09:45:37 +0200

    progress_bar

--- components/progress_bar.vue
+++ components/progress_bar.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        {{progress}}
-    </div>
+    <div class="progress-bar-container">
+        <div class="progress-bar" :style="{ width: progress + '%' }"></div>
+    </div>    
 </template>
 
 <script setup lang="ts">
 
 let progress = ref(100)
 
-onMounted(()=>{
+onMounted(()=> {
      setInterval(() => {
         if(progress.value != 0) {
-            progress.value -= 2.5
+            progress.value -= 1
         } else {
-            progress.value = 100
+            progress.value = 0
         }
      },100)  
 })
 
 </script>
+
+<style scoped>
+.progress-bar-container {
+    width: 50%;
+    height: 20px;
+    background-color: #707070; 
+    border-radius: 4px;
+    overflow: hidden;
+}
+
+.progress-bar {
+    height: 100%;
+    background-color: #ea00ff;
+    transition: width 1s linear;
+}
+</style>
