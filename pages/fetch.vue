2777e6eea8205531efcf3acaa8a9f932929c7e31
Niklas D <niklas.demel1@gmail.com>
2024-04-29 12:31:40 +0200

    convert json to quiz

--- pages/fetch.vue
+++ pages/fetch.vue
@@ -1,13 +1,24 @@
 <template>
-<div>
-    {{ dataTwice }}
+<div class="content">
+    <QuizComponent :description="exampleQuiz.description" :name="exampleQuiz.name" :questions="exampleQuiz.questions"></QuizComponent> 
 </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import {type Quiz } from "~/types/quiz";
 
 //const dataTwice = await $fetch('https://jsonplaceholder.typicode.com/posts')
-const dataTwice = await $fetch('http://localhost:3030/quizzes')
-//http://localhost:3030/ping
+//Test fetch
+const dataTwice = await fetch('http://localhost:3030/quizzes')
+let data = await dataTwice.json()
+let exampleQuiz :Quiz = data[0]
 
 </script>
+
+<style>
+.content {
+    margin-left: 100px;
+    margin-right: 100px;
+    @apply flex flex-col mt-16 justify-center items-center text-white bg-gray-700 rounded-xl p-10;
+  }
+</style>
