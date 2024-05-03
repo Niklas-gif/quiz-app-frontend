8466147c75a8ec0bb96413e4eabc3976738d23aa
Niklas D <niklas.demel1@gmail.com>
2024-05-03 09:53:44 +0200

    quiz fetch data

---
+++ components/quizlist_component.vue
@@ -0,0 +1,23 @@
+<template>
+    <li  v-for="(quiz, index) in props.quizzes">
+        {{quiz.description}}
+    </li>
+
+</template>
+
+<script setup lang="ts">
+import {type Quiz } from "../types/quiz"
+import { defineProps ,ref } from 'vue'
+
+const props = defineProps<({
+    quizzes: Array<Quiz>,
+  })>()
+
+
+
+</script>
+
+<style>
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
         <h1 class="pt-5 pb-5">Welcome to Quiz World!</h1>
     </header>
       <div class="content">
+        <QuizListComponent :quizzes="exampleQuiz"></QuizListComponent>
         <div class="flex flex-col p-5">
           <PlayButton class="self-center"></PlayButton>
           <p> Play now!</p>
@@ -18,6 +19,22 @@
 </template>
 
 <script setup lang="ts">
+ import QuizListComponent from '~/components/quizlist_component.vue'
+import type { Quiz } from '~/types/quiz';
+
+/*const dataTwice = await fetch('http://localhost:3030/quizzes')
+let data = await dataTwice.json()
+let exampleQuiz :Quiz[] = data*/
+let exampleQuiz :Ref<Quiz[]> = ref([])
+
+
+const dataTwice = await fetch('http://localhost:3030/quizzes')
+let data = await dataTwice.json()
+exampleQuiz.value = data
+/*onMounted(async ()=>{
+
+})*/
+ 
 
 </script>
 
