1a3c36e08830656a3237d841039dc6efe4b6318d
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-05-22 18:22:24 +0200

    add question wip

---
+++ components/add_quiz_button.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="button">
+        <p>Question</p>
+        <IconPlus />
+    </div>     
+
+</template>
+
+<script setup lang="ts">
+import IconPlus from "assets/icons/icon_plus.vue"
+
+</script>
+
+<style>
+.button {
+    @apply
+    flex 
+    flex-row
+    hover:bg-purple-400 
+    bg-purple-600 border-b-4 
+    border-stone-800 
+    text-white font-bold 
+    py-4 px-4 
+    my-4 mx-4 
+    rounded-3xl;
+}
+
+</style>
--- layouts/create-layout.vue
+++ layouts/create-layout.vue
@@ -2,9 +2,11 @@
     <div class="content">
         <input class="input" placeholder="Name of your quiz" type="text">
         <input class="input" placeholder="A short description of your quiz." type="text">
-        <div>TODO[ADD QUESTION]</div>
-        <div>TODO[ADD ANSWER]</div>
-        <button @click="submitQuiz">SUBMIT</button>
+        <AddQuizButton @click="addQuestion"></AddQuizButton>
+        <div v-for="(question, index) in newQuiz?.questions" :key="index">
+            <div>{{ question }}</div>
+        </div>
+        <button @click="submitQuiz">SUBMIT Test Quiz</button>
     </div>
 
 </template>
@@ -12,17 +14,41 @@
 <script setup lang="ts">
 import test_quiz from '~/testdata/fromjsontest';
 import type { Quiz } from '~/types/quiz';
-const quizData = test_quiz
+import type { Question } from '~/types/question';
+import type { Answer } from '~/types/answer';
+const testQuiz = test_quiz
+const newQuiz: Ref<Quiz | null> = ref(null)
+
+onMounted(()=>{
+    newQuiz.value = {
+        _id: "",
+        name:"",
+        description:"",
+        questions: []
+}
+})
+
+function addQuestion() {
+    const test: Question = {
+        description: "Das ist die Beschreibung",
+        is_multiple_choice: false,
+        answers:[
+            {description: "Antwort_2",is_correct: false},
+            {description: "Antwort_2",is_correct: false},
+            {description: "Antwort_2",is_correct: false}]
+        }
+    newQuiz.value?.questions.push(test)
+}
 
 async function submitQuiz(/*quiz: Quiz*/) {
-    console.log(quizData[0])
+    console.log(testQuiz[0])
     try {
         const response = await fetch('http://localhost:3030/add', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(quizData[0])
+            body: JSON.stringify(testQuiz[0])
         })
         if (response.ok) {
             console.log(response);
