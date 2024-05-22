0d05c7c44ad23a3ad6c89cc63fe1dec8052b92b3
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-05-22 18:57:07 +0200

    add Answer WIP

--- layouts/create-layout.vue
+++ layouts/create-layout.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="content">
-        <input class="input" placeholder="Name of your quiz" type="text">
-        <input class="input" placeholder="A short description of your quiz." type="text">
-        <AddQuizButton @click="addQuestion"></AddQuizButton>
+        <input class="input" placeholder="Name of your quiz" v-model="newQuiz.name">
+        <input class="input" placeholder="A short description of your quiz." v-model="newQuiz.description">
+        <AddQuizButton @click="createQuestion"></AddQuizButton>
+        <div v-if="newQuestion != null" class="flex flex-col bg-slate-900 rounded-lg">
+            <input class="input" placeholder="Name of your quiz" v-model="newQuestion.description">
+            <button @click="addAnswer"> Add Answer</button>
+            <button> Toggle </button>
+            <button @click="addQuestion"> Submit </button>
+        </div>
         <div v-for="(question, index) in newQuiz?.questions" :key="index">
             <div>{{ question }}</div>
         </div>
@@ -17,38 +23,46 @@ import type { Quiz } from '~/types/quiz';
 import type { Question } from '~/types/question';
 import type { Answer } from '~/types/answer';
 const testQuiz = test_quiz
-const newQuiz: Ref<Quiz | null> = ref(null)
+const newQuiz: Ref<Quiz> = ref<Quiz>({
+    _id: "",
+    name: "",
+    description: "",
+    questions: []
+});
 
-onMounted(()=>{
-    newQuiz.value = {
-        _id: "",
-        name:"",
-        description:"",
-        questions: []
+const newQuestion: Ref<Question | null> = ref(null)
+
+function createQuestion() {
+    newQuestion.value = {
+        description: "",
+        is_multiple_choice: false,
+        answers: [],
+    }
 }
-})
 
 function addQuestion() {
-    const test: Question = {
-        description: "Das ist die Beschreibung",
-        is_multiple_choice: false,
-        answers:[
-            {description: "Antwort_2",is_correct: false},
-            {description: "Antwort_2",is_correct: false},
-            {description: "Antwort_2",is_correct: false}]
-        }
-    newQuiz.value?.questions.push(test)
+    newQuiz.value?.questions.push(newQuestion.value!)
+    newQuestion.value = null
+}
+
+function addAnswer() {
+    newQuestion.value!.answers =  [
+        {description:"Antwort 1",is_correct: false},
+    {description:"Antwort 1",is_correct: false},
+    {description:"Antwort 3 ist Richtig",is_correct: true},
+]
+
 }
 
 async function submitQuiz(/*quiz: Quiz*/) {
-    console.log(testQuiz[0])
+    console.log(newQuiz)
     try {
         const response = await fetch('http://localhost:3030/add', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(testQuiz[0])
+            body: JSON.stringify(newQuiz.value)
         })
         if (response.ok) {
             console.log(response);
