9837fe849a0371170aa9875a7f81e07b002358b4
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-05-27 10:14:13 +0200

    v-show to prevent hydration error

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="content">
       <div class="flex flex-col lg:flex-row mx-10 px-10">
-        <img v-if="!showList" src="/assets/images/game_console.png" class="max-h-96">
+        <img v-show="!showList" src="/assets/images/game_console.png" class="max-h-96">
           <div class="flex flex-col">
-            <h1 v-if="!showList" class="self-center text-xl font-bold">Welcome to Quiz World!</h1>
-            <div v-if="!showList" class="flex flex-col p-5">
+            <h1 v-show="!showList" class="self-center text-xl font-bold">Welcome to Quiz World!</h1>
+            <div v-show="!showList" class="flex flex-col p-5">
                 <PlayButton class="self-center" @click="toggleList"></PlayButton>
                <p class="self-center"> Play now!</p>
             </div>
@@ -12,7 +12,7 @@
                <IconLoading v-if="isLoading"></IconLoading>
                 <QuizListComponent v-else  :quizzes="data"/>
             </div>
-              <NuxtLink v-if="!showList" to="/create">  
+              <NuxtLink v-show="!showList" to="/create">  
                 <div class="flex flex-col p-5">
                   <CreateButton class="self-center"></CreateButton>
                   <p class="self-center"> Create </p>
