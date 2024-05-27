b32128870d0731fa31fb751efbf65517872e1db5
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-05-27 15:11:38 +0200

    questions know can be removed

--- layouts/create-layout.vue
+++ layouts/create-layout.vue
@@ -4,26 +4,37 @@
         <input class="input m-2" placeholder="A short description of your quiz." v-model="newQuiz.description">
         <AddQuizButton @click="createQuestion"/>
 
-        <div v-for="question in newQuestions" class="flex flex-col bg-slate-900 rounded-lg p-5 space-y-5">
-            <input class="input" placeholder="Question description" v-model="question.description">
+        <div class="flex flex-row space-x-5">
+        <div v-for="question in newQuestions" class="flex flex-col bg-slate-900 rounded-lg p-5 mb-5 space-y-5">
+            <div class="flex flex-row">
+                <input class="input" placeholder="Question description" v-model="question.description">
+                <button @click="removeQuestion(question)" class="remove-button">
+                    <IconTrash></IconTrash>
+                </button>
+            </div>
             <div class="flex flex-row space-x-2">
                 <label for="isMultipleChoice">Multiple choice question</label>
                 <input type="checkbox" id="isMultipleChoice" v-model="question.is_multiple_choice" />
             </div>
-            <button @click="createAnswer(question)" class="add-answer-button"> New Answer</button>
 
             <div v-if="question.answers.length != 0" class="flex flex-col">
                 <div v-for="(answer) in question.answers">
-                    <input class="input" placeholder="Answer" v-model="answer.description">
+                    <div class="flex flex-row">
+                        <input class="input" placeholder="Answer" v-model="answer.description">
+                        <button class="remove-button" @click="removeAnswer(question,answer)">
+                            <IconTrash></IconTrash>
+                        </button>
+                    </div>
                     <div class="flex flex-row space-x-2">
                         <label for="is_correct">Is true</label>
                         <input type="checkbox" id="is_correct" v-model="answer.is_correct" />
-                        <button class="remove-button" @click="removeAnswer(question,answer)">Remove answer</button>
                     </div>
                 </div>
             </div>
-            <div v-else> IST NULL</div>
 
+            <button @click="createAnswer(question)" class="add-answer-button"> New Answer</button>
+
+        </div>
         </div>
 
         <div v-for="(question, index) in newQuiz?.questions" :key="index" class="flex flex-col">
@@ -40,6 +51,7 @@
 import type { Quiz } from '~/types/quiz';
 import type { Question } from '~/types/question';
 import type { Answer } from '~/types/answer';
+import IconTrash from "assets/icons/icon_trash.vue"
 
 const newQuiz: Ref<Quiz> = ref<Quiz>({
     _id: "",
@@ -63,6 +75,10 @@ function addQuestions() {
         newQuiz.value.questions.push(question)
     })
     newQuestions.value = []
+}
+
+function removeQuestion(questionToRemove: Question) {
+    newQuestions.value = newQuestions.value.filter(question => question !== questionToRemove)
 }
 
 function createAnswer(question: Question) {
@@ -104,7 +120,7 @@ async function submitQuiz(/*quiz: Quiz*/) {
   }
 
 .input {
-    @apply p-2  bg-slate-500 rounded-lg
+    @apply p-2  bg-slate-500 rounded-lg w-48 flex-shrink-0
 }
 
 .submit-button {
@@ -124,7 +140,7 @@ async function submitQuiz(/*quiz: Quiz*/) {
     border-stone-800 
     text-white font-bold 
     p-4 m-5
-    rounded-3xl;
+    rounded-full;
 }
 
 
