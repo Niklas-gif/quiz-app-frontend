b92008c8e696f35400f6d7442fc546d6c2712e09
Niklas D <niklas.demel1@gmail.com>
2024-05-28 12:33:38 +0200

    wip create quiz component

--- components/create_quiz_component.vue
+++ components/create_quiz_component.vue
@@ -1,12 +1,6 @@
 <template>
-    <div class="content">
-        <input class="input m-2" placeholder="Name of your quiz" v-model="newQuiz.name">
-        <input class="input m-2" placeholder="A short description of your quiz." v-model="newQuiz.description">
-        <AddQuizButton @click="createQuestion"/>
-       <!-- <CreateQuizComponent :questions="newQuestions"></CreateQuizComponent>-->
-
-        <div class="grid grid-cols-1 lg:grid-cols-3 space-x-5">
-        <div v-for="question in newQuestions" class="flex flex-col bg-slate-900 rounded-lg p-5 mb-5 space-y-5">
+    <div class="grid grid-cols-1 lg:grid-cols-3 space-x-5">
+        <div v-for="question in questions" class="flex flex-col bg-slate-900 rounded-lg p-5 mb-5 space-y-5">
             <div class="flex flex-row">
                 <input class="input" placeholder="Question description" v-model="question.description">
                 <button @click="removeQuestion(question)" class="remove-button">
@@ -38,65 +32,42 @@
         </div>
         </div>
 
-        <div v-for="(question, index) in newQuiz?.questions" :key="index" class="flex flex-col">
-            <div>Description: {{ question.description }}</div>
-            <div>Is multiple choice: {{ question.is_multiple_choice }}</div>
-            <div>Answers: {{ question.answers }}</div>
-        </div>
-        <button class="submit-button" @click="submitQuiz">SUBMIT</button>
-    </div>
-
 </template>
 
 <script setup lang="ts">
-import type { Quiz } from '~/types/quiz';
-import type { Question } from '~/types/question';
-import type { Answer } from '~/types/answer';
-import CreateQuizComponent from '~/components/create_quiz_component.vue'
+import { type Answer } from '../types/answer'
+import {type Question} from '../types/question'
 import IconTrash from "assets/icons/icon_trash.vue"
 
-const newQuiz: Ref<Quiz> = ref<Quiz>({
-    _id: "",
-    name: "",
-    description: "",
-    questions: []
-});
+const props = defineProps<({
+    questions: Question[],
+  })>();
 
-const newQuestions: Ref<Question[]> = ref([])
-
-function createQuestion() {
-    newQuestions.value.push ({
-        description: "",
-        is_multiple_choice: false,
-        answers: [],
-    })
-}
-
-function addQuestions() {
-    newQuestions.value.forEach(question => {
+  function addQuestions() {
+    /*newQuestions.value.forEach(question => {
         newQuiz.value.questions.push(question)
     })
-    newQuestions.value = []
+    newQuestions.value = []*/
 }
 
 function removeQuestion(questionToRemove: Question) {
-    newQuestions.value = newQuestions.value.filter(question => question !== questionToRemove)
+    //newQuestions.value = newQuestions.value.filter(question => question !== questionToRemove)
 }
 
 function createAnswer(question: Question) {
-    question.answers.push({
+    /*question.answers.push({
         description: "",
         is_correct: false
-    })
+    })*/
 }
 
 
 function removeAnswer(question: Question, answerToRemove: Answer) {
-    question.answers = question.answers.filter(answer => answer !== answerToRemove)
+    //question.answers = question.answers.filter(answer => answer !== answerToRemove)
 }
 
 async function submitQuiz() {
-    addQuestions()
+   /* addQuestions()
     try {
         const token = localStorage.getItem('Bearer')
         const response = await fetch('http://localhost:3030/add', {
@@ -112,8 +83,9 @@ async function submitQuiz() {
         }
     } catch (error) {
         console.error('Error sending quiz data:', error);
-    }
+    }*/
 }
+
 
 </script>
 
