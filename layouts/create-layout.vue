c01e58bf726b80f91d896f73b98a914176550a35
Niklas D <niklas.demel1@gmail.com>
2024-05-30 10:51:16 +0200

    load quiz onmounted

--- layouts/create-layout.vue
+++ layouts/create-layout.vue
@@ -62,6 +62,23 @@ const newQuiz: Ref<Quiz> = ref<Quiz>({
     questions: []
 });
 
+const route = useRoute()
+
+const isLoading = ref(false)
+
+onMounted(async () => {
+    isLoading.value = true
+    try {
+      const response = await fetch(`http://localhost:3030/quizzes/${route.query.currentQuiz}`)
+      const data = await response.json()
+      newQuiz.value = data[0]
+      console.log(newQuiz.value)
+    } catch(error) {
+      console.log(error)
+    }
+    isLoading.value = false
+})
+
 const newQuestions: Ref<Question[]> = ref([])
 
 function createQuestion() {
