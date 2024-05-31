79162592ccdb5fc947cc0b22500395f00ab4ca10
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-05-31 21:59:48 +0200

    Update quiz-layout.vue

--- layouts/quiz-layout.vue
+++ layouts/quiz-layout.vue
@@ -1,18 +1,15 @@
 <template>
-    <div class="content">
-      <p v-if="!isRunning"> GAME OVER!</p>
-      <p>{{attributes.quiz.name}}</p>
-      <p>{{attributes.quiz.id}}</p>
-      <NuxtLink to="/" class="p-5 hover:text-purple-400">Go back to main</NuxtLink>
-      <ProgressBar></ProgressBar>
-      <QuestionComponent 
-        :selected-answers="selectedAnswers" 
-        :answers="currentQuesiton.answers" 
-        :description="currentQuesiton.description"
-        @answerSelected="selectCard">
-      </QuestionComponent>
-      <button @click="nextQuestion"> next question </button>
-    </div>
+  <div class="content">
+    <p v-if="!isRunning"> GAME OVER!</p>
+    <p>{{ attributes.quiz.name }}</p>
+    <p>{{ attributes.quiz.id }}</p>
+    <NuxtLink to="/" class="p-5 hover:text-purple-400">Go back to main</NuxtLink>
+    <ProgressBar></ProgressBar>
+    <QuestionComponent :selected-answers="selectedAnswers" :answers="currentQuesiton.answers"
+      :description="currentQuesiton.description" @answerSelected="selectCard">
+    </QuestionComponent>
+    <button @click="nextQuestion"> next question </button>
+  </div>
 
 </template>
 
@@ -20,39 +17,54 @@
 import QuestionComponent from '~/components/question_component.vue'
 import ProgressBar from '~/components/progress_bar.vue'
 import { type Answer } from '~/types/answer'
+import { type Question } from '~/types/question'
 
-const attributes= useAttrs() as any
+const attributes = useAttrs() as any
 
 let selectedAnswers: Ref<Answer[]> = ref([])
-let currentQuesiton = ref(attributes.quiz.questions[0])
+let currentQuesiton: Ref<Question> = ref(attributes.quiz.questions[0])
 let index = ref(0)
 let isRunning = ref(true)
 
 function selectCard(answer: Answer) {
-//TODO check if question is multipblechoice
-  if(selectedAnswers.value.includes(answer)) {
-    selectedAnswers.value.splice(selectedAnswers.value.indexOf(answer), 1) 
+  if (selectedAnswers.value.includes(answer)) {
+    selectedAnswers.value.splice(selectedAnswers.value.indexOf(answer), 1)
   } else {
-      selectedAnswers.value.push(answer)
-      setTimeout(() => {
-      if(selectedAnswers.value.length != 0) { 
+
+    if (!currentQuesiton.value.is_multiple_choice && selectedAnswers.value.length == 1) {
+      return
+    }
+    selectedAnswers.value.push(answer)
+    setTimeout(() => {
+      if (selectedAnswers.value.length != 0) {
         submitAnswer(answer)
-      } 
-    },1000)
-}
-  
+      }
+    }, 1000)
+  }
+
 }
 
 function submitAnswer(answer: Answer) {
 
-  if(answer.is_correct) {
+  if(answer.is_correct && !currentQuesiton.value.is_multiple_choice) {
     nextQuestion()
     selectedAnswers.value = []
+    return
+  }
+
+  if(currentQuesiton.value.is_multiple_choice) {
+    let i = 0
+    selectedAnswers.value.forEach(answer => {
+      if(answer.is_correct) {
+        i++
+      }
+      console.log(i,"out of",currentQuesiton.value.answers.length,"are correct!")
+    });
   }
 }
 
 function nextQuestion() {
-  if(index.value + 1 < attributes.quiz.questions.length) {
+  if (index.value + 1 < attributes.quiz.questions.length) {
     currentQuesiton.value = attributes.quiz.questions[index.value += 1]
   } else {
     isRunning.value = false;
