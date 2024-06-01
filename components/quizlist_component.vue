7d45daeed70892af82e57c5f721eaa3993560543
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-06-01 12:40:59 +0200

    master detailview wip

--- components/quizlist_component.vue
+++ components/quizlist_component.vue
@@ -1,16 +1,27 @@
-<template> 
-<ul>
-    <li  v-for="(quiz, index) in props.quizzes" :key="index" @click="toggleDetails(index)" class="item">
-         {{quiz.name}}   
-        <span v-if="visibleItems[index]" class="flex flex-col">
+<template>
+  <div class="flex flex-row">
+    <ul>
+      <li v-for="(quiz, index) in currentQuizzes" :key="index" @click="selectQuiz(quiz)" class="item"
+        :class="{ 'selected': quiz == selectedQuiz }">
+        {{ quiz.name }}
+        <!--<span v-if="quiz == selectedQuiz" class="flex flex-row">
             {{quiz.description}}
             <NuxtLink :to="{ path: path, query: { currentQuiz: quiz.name }}">
               <button class="button">Play now</button>
             </NuxtLink>
             <button @click="deleteQuiz(quiz)"> Delete Quiz</button>
-        </span>
-    </li>
-</ul>
+        </span>-->
+      </li>
+    </ul>
+    <div>
+      <p>{{ selectedQuiz.name }}</p>
+      <p>{{selectedQuiz.description}}</p>
+      <NuxtLink :to="{ path: path, query: { currentQuiz: selectedQuiz.name }}">
+        <button class="button">Play now</button>
+      </NuxtLink>
+      <button class="button-delete" @click="deleteQuiz(selectedQuiz)"> Delete Quiz</button>
+    </div>
+  </div>
 
 </template>
 
@@ -21,15 +32,25 @@ import { defineProps, ref } from 'vue'
 const props = defineProps<({
     quizzes: Array<Quiz>,
     path: string,
-
-    //TODO Path and query as prop?
   })>()
 
-const visibleItems = ref(Array(props.quizzes.length).fill(false));
+//const visibleItems = ref(Array(props.quizzes.length).fill(false));
+const selectedQuiz: Ref<Quiz> = ref(props.quizzes[0]) 
+const currentQuizzes: Ref<Quiz[]> =  ref(props.quizzes)
 
-function toggleDetails(index: number) {
+
+//TODO Delete quiz for the current state!!!
+/*function toggleDetails(index: number) {
   visibleItems.value[index] = ! visibleItems.value[index]
+}*/
+
+function selectQuiz(quiz: Quiz) {
+  selectedQuiz.value = quiz
 }
+
+/*function removeQuizFromList(quizToDelete: Quiz) {
+  currentQuizzes.value = currentQuizzes.value.filter(quiz => { quizToDelete == quiz })
+}*/
 
 async function deleteQuiz(quiz: Quiz) {
     try {
@@ -44,6 +65,8 @@ async function deleteQuiz(quiz: Quiz) {
         })
         if (response.ok) {
             console.log(response);
+            //removeQuizFromList(selectedQuiz.value)
+            selectedQuiz.value = currentQuizzes.value[0]
         }
     } catch (error) {
         console.error('Error sending quiz data:', error);
@@ -54,11 +77,19 @@ async function deleteQuiz(quiz: Quiz) {
 
 <style scoped>
 .item {
- @apply text-white bg-orange-600 rounded-3xl p-5 m-5 hover:bg-orange-500 hover:cursor-pointer
+ @apply text-white bg-orange-600 hover:bg-orange-500 hover:cursor-pointer;
+}
+
+.item.selected {
+  @apply bg-orange-800 hover:bg-orange-700
 }
 
 .button {
   @apply hover:bg-purple-400 bg-purple-600 border-b-4 border-stone-800 text-white font-bold py-4 px-4 rounded-3xl hover:cursor-pointer;
+}
+
+.button-delete {
+  @apply hover:bg-red-400 bg-red-600 border-b-4 border-stone-800 text-white font-bold py-4 px-4 rounded-3xl hover:cursor-pointer;
 }
 
 
