faee019e8d52f0640944f6e395b08bac251101f8
Niklas D <niklas.demel1@gmail.com>
2024-06-03 10:59:57 +0200

    detail view

---
+++ components/detail_component.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>{{props.quiz.name}}</h2>
+        <p>{{props.quiz.description}}</p>
+        <p>Amount of questions:{{questionCount}}</p>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import type { Quiz } from '~/types/quiz';
+
+const props = defineProps<({
+    quiz: Quiz,
+  })>()
+
+  const questionCount = computed(() => {
+  return props.quiz.questions.length
+})
+
+</script>
+
+<style scoped>
+
+.quiz-title {
+    @apply text-3xl font-bold truncate
+  }
+
+</style>
