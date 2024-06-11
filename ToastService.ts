9bfb2b82b3c33802dfe17012d795882b32ddd908
Niklas D <niklas.demel1@gmail.com>
2024-06-11 08:14:11 +0200

    generic toasts

--- ToastService.ts
+++ ToastService.ts
@@ -1,7 +1,32 @@
-import { useToast } from "vue-toast-notification";
+import { useToast } from "vue-toast-notification"
 import 'vue-toast-notification/dist/theme-sugar.css'
+
 export class ToastService {
     private $toast = useToast()
+
+    error(message: string) {
+        let instance = this.$toast.error(message)
+            setTimeout(()=>{
+                instance.dismiss()
+                this.$toast.clear()
+            },5000) 
+    }
+
+    success(message: string) {
+        let instance = this.$toast.success(message)
+            setTimeout(()=>{
+                instance.dismiss()
+                this.$toast.clear()
+            },5000)
+    } 
+
+    warning(message: string) {
+        let instance = this.$toast.warning(message)
+            setTimeout(()=>{
+                instance.dismiss()
+                this.$toast.clear()
+            },5000)
+    } 
 
     wrongLoginCredentials() {
         let instance = this.$toast.error('Password or Email is invalid!')
