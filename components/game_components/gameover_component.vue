30c0798bb5f99c6c063f48137a7455c4fea44be8
Niklas D <niklas.demel1@gmail.com>
2024-06-11 13:16:08 +0200

    Update gameover_component.vue

--- components/game_components/gameover_component.vue
+++ components/game_components/gameover_component.vue
@@ -1,24 +1,35 @@
 <template>
-    <div>
-        <p> {{props.correctQuestions}} out of {{questions.length}} are Correct </p>
-        <NuxtLink to="/">
-            <p>go back</p>
-        </NuxtLink>
+    <div class="container">
+        <h1 class="title">GAME OVER!</h1>
+      <p class="text-lg font-bold mb-4">
+        {{ props.correctQuestions }} out of {{ props.questions.length }} questions are Correct
+      </p>
+      <NuxtLink to="/" class="goback">
+        <p>Go back</p>
+      </NuxtLink>
     </div>
-    
-</template>
-
-<script setup lang="ts">
-import type { Question } from '~/types/question';
-
-
-const props = defineProps<({
+  </template>
+  
+  <script setup lang="ts">
+  import type { Question } from '~/types/question';
+  
+  const props = defineProps<{
     correctQuestions: number,
     questions: Question[]
-  })>()
+  }>();
+  </script>
+  
+  <style scoped>
+  .title {
+    @apply text-center text-4xl font-bold animate-pulse mb-5
+  }
 
-</script>
+  .container {
+    @apply flex flex-col text-sky-400 items-center justify-center p-6;
+  }
 
-<style scoped>
-
-</style>
+  .goback {
+    @apply text-sky-400 hover:text-sky-200 bg-slate-700 rounded-lg p-5
+  }
+  </style>
+  
