6f246ed4e869e25c18a10bddbeb08081ea2b18c6
Niklas D <niklas.demel1@gmail.com>
2024-07-11 21:13:00 +0200

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,24 +5,32 @@ export default defineNuxtConfig({
       BACKEND_URL: "http://localhost:3030/"
     }
   },
+
+  // other configs 
   devServer: {     
     port: 3000, // default: 3000     
     host: '0.0.0.0', // default: localhost   
-  },   // other configs 
+  },
+
   app: {
     pageTransition: { name: 'page', mode:'in-out'},
     layoutTransition: {name: 'layout', mode:'in-out'}
   },
+
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   pages: true,
   ssr: false,
+
   hooks: {
     "prerender:routes"({routes}) {
       routes.clear()
     }
   },
+
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+
+  compatibilityDate: '2024-07-11'
 })
